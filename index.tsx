244f9a993225e5c5cd32e96c8eaaf4f7c2f4e935
liangxin <liangxin@mail.teletraan.io>
2025-07-11 16:06:21 +0800

    feat: 初始化

---
+++ index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const VirtualList = () => {
+  return <div>1</div>;
+};
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./global.css";
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
